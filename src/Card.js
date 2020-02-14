b0f159177d27bd709e46a44d6ac16e7877fca811
Takeshi Kimoto <lnuk2009jp@gmail.com>
2020-02-14 00:09:06 +0000

    description added

--- src/Card.js
+++ src/Card.js
@@ -3,19 +3,22 @@ import PropTypes from 'prop-types';
 
 const Card = ({whsite}) => {
     // const {index, picture, city, address, bedrooms, bathrooms, carSpaces} = site;
-    const {index, id, site, image_url, states, http_url} = whsite;
+    const {index, id, site, image_url, states, http_url, short_description} = whsite;
     
     return (
         <div id={`card-${index}`} className="card">
-           <a href={http_url}><img src={image_url} alt={id}/></a>
+           <img src={image_url} alt={id}/>
             <div className="details">
                 <span className="index"></span>
-                <p className="location">
+                <a href={http_url}rel="noopener noreferrer" target="_blank"><p className="location">
                     {site}
-                </p>
+                </p></a>
                 <ul className="features">
                     <li className="icon-house">{states} <span>Country</span></li>
                 </ul>
+                <p>
+                    {short_description}
+                </p>
             </div>
         </div>
     )
